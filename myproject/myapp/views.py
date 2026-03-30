535f220ad2874e40c8e6e4a3a84ecad9aa76cfa0
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-03-30 19:08:16 +0530

    Built a word counter using views and templates

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -8,4 +8,10 @@ def index(request):
     age=11
     nationality='Indian'
     context={'name':name, 'age': age,'nationality': nationality}
-    return render(request,'index.html',context)
+    return render(request,'index.html')
+
+def counter(request):
+    text=request.GET['text']
+    amount_of_words=len(text.split(" "))
+    return render(request, 'counter.html',{'amount': amount_of_words})
+
