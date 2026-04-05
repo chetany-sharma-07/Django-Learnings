b63a2d7e8131d6fdf09a0f3a7be2756f170ec5d5
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-04-05 18:09:43 +0530

    Used Django template loop to dynamically render feature objects

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -24,7 +24,9 @@ def index(request):
     feature4.id=3
     feature4.name='Affordable'
     feature4.details='Our Service is very Affordable.'
-    return render(request,'index.html',{'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})
+
+    features=[feature1, feature2, feature3, feature4]
+    return render(request,'index.html',{'features':features})
 
 def counter(request):
     text=request.POST['text']
