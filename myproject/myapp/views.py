e236f152abef3d88fd839d781b72e89718cc53b9
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-04-05 17:19:19 +0530

    Added Feature model and dynamic data rendering in template

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 def index(request):
-    name='Chetany'
-    age=11
-    nationality='Indian'
-    context={'name':name, 'age': age,'nationality': nationality}
-    return render(request,'index.html')
+    feature1=Feature()
+    feature1.id=0
+    feature1.name='Fast'
+    feature1.details='Our Service is very Fast.'
+
+    feature2=Feature()
+    feature2.id=1
+    feature2.name='Easy'
+    feature2.details='Our Service is very easy to use.'
+
+    feature3=Feature()
+    feature3.id=2
+    feature3.name='Reliable'
+    feature3.details='Our Service is very Reliable.'
+
+    feature4=Feature()
+    feature4.id=3
+    feature4.name='Affordable'
+    feature4.details='Our Service is very Affordable.'
+    return render(request,'index.html',{'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})
 
 def counter(request):
     text=request.POST['text']
