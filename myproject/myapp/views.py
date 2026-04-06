47357c7e92232fa9379da63ee4b6d8841146cef1
chetany-sharma-07 <sharmachetany1004@gmail.com>
2026-04-06 20:56:13 +0530

    Integrated Django ORM: converted Feature to model, applied migrations, added admin support, and rendered database data in template.

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,27 +5,7 @@ from .models import Feature
 # Create your views here.
 
 def index(request):
-    feature1=Feature()
-    feature1.id=0
-    feature1.name='Fast'
-    feature1.details='Our Service is very Fast.'
-
-    feature2=Feature()
-    feature2.id=1
-    feature2.name='Easy'
-    feature2.details='Our Service is very easy to use.'
-
-    feature3=Feature()
-    feature3.id=2
-    feature3.name='Reliable'
-    feature3.details='Our Service is very Reliable.'
-
-    feature4=Feature()
-    feature4.id=3
-    feature4.name='Affordable'
-    feature4.details='Our Service is very Affordable.'
-
-    features=[feature1, feature2, feature3, feature4]
+    features=Feature.objects.all()
     return render(request,'index.html',{'features':features})
 
 def counter(request):
